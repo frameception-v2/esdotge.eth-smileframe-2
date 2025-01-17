6b2879888b7be30ac171083884018b841fa73348
hellno <686075+hellno@users.noreply.github.com>
2025-01-17 22:20:07 +0000

    refactor: Update project constants to reflect new branding and purpose

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const PROJECT_ID = 'farcaster-frames-template';
-export const PROJECT_TITLE = "Farcaster Frames Template";
-export const PROJECT_DESCRIPTION = "A Farcaster Frames v2 Template by hellno";
+export const PROJECT_ID = 'smileframe';
+export const PROJECT_TITLE = "SmileFrame 😊";
+export const PROJECT_DESCRIPTION = "Generate smiles, share joy, earn $MILE!";
